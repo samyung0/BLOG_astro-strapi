d044594ab652634faf53d2fe730a52d35a36d8e6
sam <yungchinpang999@gmail.com>
2023-12-27 12:23:44 +0800

    added sitemap for astro

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import sitemap from '@astrojs/sitemap';
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), sitemap()],
   prefetch: {
     prefetchAll: true
   }
